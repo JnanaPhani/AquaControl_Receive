dffdd40f46605d491e775587ce3559d5240980f2
phani_admin <jnanaphani.a@zenithtek25.onmicrosoft.com>
2025-04-13 14:14:31 +0530

    optimised receiver.h file

--- main/inc/receiver.h
+++ main/inc/receiver.h
@@ -1,20 +1,32 @@
 #ifndef __RECEIVER_H__
 #define __RECEIVER_H__
 
-#include <utils.h>
+// Standard libraries
+#include <sys/time.h>
+
+// ESP-IDF libraries
 #include "esp_timer.h"
 #include "esp_app_desc.h"
-#include <sys/time.h>
 #include "esp_heap_caps.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "esp_partition.h"
 #include "esp_system.h"
 #include "spi_flash_mmap.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 
-void app_main(void);
-void print_system_memory_status();
+// FreeRTOS
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-#endif /*__RECEIVER_H__*/
+// Project-specific
+#include <utils.h>
+
+// Function declarations
+void app_main(void);
+
+/**
+ * @brief Prints current system memory status.
+ */
+void print_system_memory_status(void);
+
+#endif /* __RECEIVER_H__ */
